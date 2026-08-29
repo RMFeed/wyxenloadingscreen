275f79f93b574aab10d360dae731531d2c1812eb
alagozyavuz343 <alagozyavuz343@gmail.com>
2026-08-29 03:37:50 +0300

    Revert "Alt bari cercevenin hemen altina sabitle"
    This reverts commit 00bf415852f82a224594b90aad7d013ad5e5fc14.

--- js/ui.js
+++ js/ui.js
@@ -394,10 +394,7 @@
         journey.style.width = journeyWidth + "px";
         journey.style.height = rowHeight + "px";
         journey.style.right = "auto";
-        // Bar sayfanın altına değil, doğrudan çerçevenin altına oturuyor;
-        // aksi halde uzun ekranlarda arada ölü boşluk kalıyor.
-        journey.style.top = (FRAME_RING + height + FRAME_GAP) + "px";
-        journey.style.bottom = "auto";
+        journey.style.bottom = FRAME_RING + "px";
       }
     }
   }
